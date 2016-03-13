6da8050420de8ef81cd5ec2728e0ca4fb82336c7
LKHohn <lkhohn@gmail.com>
2016-03-13 16:16:18 -0600

    working on ionic paths for auth

--- fetchApp/www/js/controllers.js
+++ fetchApp/www/js/controllers.js
@@ -4,7 +4,7 @@ angular.module('starter.controllers', [])
 
 .controller('FindFetchCtrl', function($scope) {})
 
-.controller('AccountCtrl', ['$scope', '$location', 'SigninService', AccountCtrl])
+.controller('AccountCtrl', ['$scope', '$location', 'Password', 'SigninService', 'AddUserService', AccountCtrl])
 
 .controller('AddFetchCtrl', ['$scope', '$location', 'FetchService', AddFetchCtrl]);
 
@@ -34,59 +34,62 @@ function AddFetchCtrl($scope, $location, FetchService) {
 function AccountCtrl($scope, $location, Password, SigninService, AddUserService){
   var vm = this;
   vm.signin = signin;
+  vm.signup = signup;
+
   function signin(user){
     SigninService.signin(user).then(function(response){
       localStorage.setItem('Authorization', 'Bearer ' + response.data.token);
-      $location.path('/');
+      $location.path('/tab/home');
+      vm.loggedStatus = true;
     });
   }
 
-    vm.signup = signup;
-    function signup(user) {
-      // console.log(user);
-      AddUserService.signup(user).then(function(response){
-        $location.path('/signin');
+  function signup(user) {
+
+        console.log(user);
+        AddUserService.signup(user).then(function(response){
+          $location.path('/tab/account');
+        });
+
+      this.regex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+
+      $scope.$watch('user.password', function(pass) {
+        // console.log(Password.getStrength(pass))
+      	$scope.passwordStrength = Password.getStrength(pass);
+
+      	if($scope.isPasswordWeak()) {
+          if($scope.form !== undefined && $scope.form.password !== undefined){
+      		    $scope.form.password.$setValidity('strength', false);
+          }
+          // else {
+          //   $scope.form.password = "";
+          //   $scope.form.password.$setValidity('strength', false);
+          // }
+      	} else {
+      		$scope.form.password.$setValidity('strength', true);
+      	}
       });
-    }
 
-    this.regex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
-
-    $scope.$watch('user.password', function(pass) {
-      // console.log(Password.getStrength(pass))
-    	$scope.passwordStrength = Password.getStrength(pass);
-
-    	if($scope.isPasswordWeak()) {
+      $scope.isPasswordWeak = function() {
         if($scope.form !== undefined && $scope.form.password !== undefined){
-    		    $scope.form.password.$setValidity('strength', false);
+          return $scope.passwordStrength < 20;
         }
-        // else {
-        //   $scope.form.password = "";
-        //   $scope.form.password.$setValidity('strength', false);
-        // }
-    	} else {
-    		$scope.form.password.$setValidity('strength', true);
-    	}
-    });
+        else {
+          return true;
+        }
 
-    $scope.isPasswordWeak = function() {
-      if($scope.form !== undefined && $scope.form.password !== undefined){
-        return $scope.passwordStrength < 20;
-      }
-      else {
-        return true;
-      }
-
-    };
-    $scope.isPasswordOk = function() {
-    	return $scope.passwordStrength >= 20 && $scope.passwordStrength <= 50;
-    };
-    $scope.isPasswordStrong = function() {
-    	return $scope.passwordStrength > 50;
-    };
-    $scope.isInputValid = function(input) {
-    	return input.$dirty && input.$valid;
-    };
-    $scope.isInputInvalid = function(input) {
-    	return input.$dirty && input.$invalid;
-    };
-  }
+      };
+      $scope.isPasswordOk = function() {
+      	return $scope.passwordStrength >= 20 && $scope.passwordStrength <= 50;
+      };
+      $scope.isPasswordStrong = function() {
+      	return $scope.passwordStrength > 50;
+      };
+      $scope.isInputValid = function(input) {
+      	return input.$dirty && input.$valid;
+      };
+      $scope.isInputInvalid = function(input) {
+      	return input.$dirty && input.$invalid;
+      };
+    }
+}
