68b6429790d8ca4bb7b9f4a5f0f99f2ef72b2074
Ethan Weeks <ethanweeks@ymail.com>
2016-10-01 17:49:57 -0600

    added hockey app build shell script

--- fetchApp/script.sh
+++ fetchApp/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cordova build --release android
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /Users/lindsayhohn/fetch_clientSide/fetchApp/platforms/android/build/outputs/apk/fetch-release-key.keystore /Users/lindsayhohn/fetch_clientSide/fetchApp/platforms/android/build/outputs/apk/android-release-unsigned.apk fetch_alias
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore platforms/android/build/outputs/apk/fetch-release-key.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk fetch_alias
